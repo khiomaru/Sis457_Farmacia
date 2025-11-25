0146d123d9155737259cde9581aed22619faf634
Manuel Adolfo Soto <sotomanueladolfo1@gmail.com>
2025-11-25 08:51:46 -0400

    Se agrego CAD, añadiendo la coneccion a la base de datos

---
+++ Farmacia/CadFarmacia/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CadFarmacia
+{
+    public static class Conexion
+    { 
+        private static readonly string cs = ConfigurationManager.ConnectionStrings["Labsis457Farmacia"].ConnectionString;
+
+
+    public static SqlConnection Abrir()
+        {
+            var cn = new SqlConnection(cs);
+            cn.Open();
+            return cn;
+        }
+    }
+}
