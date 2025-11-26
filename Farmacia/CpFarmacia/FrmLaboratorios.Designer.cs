eb17e884bec07aaf792ea8f3a05c81e27ff4a548
Manuel Adolfo Soto <sotomanueladolfo1@gmail.com>
2025-11-26 00:04:52 -0400

    Se agrego FrmLaboratorios

---
+++ Farmacia/CpFarmacia/FrmLaboratorios.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CpFarmacia
+{
+    partial class FrmLaboratorios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbBuscar = new System.Windows.Forms.GroupBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.dgvLaboratorios = new System.Windows.Forms.DataGridView();
+            this.gbDatos = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.gbBuscar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLaboratorios)).BeginInit();
+            this.gbDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbBuscar
+            // 
+            this.gbBuscar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.gbBuscar.Controls.Add(this.btnBuscar);
+            this.gbBuscar.Controls.Add(this.txtBuscar);
+            this.gbBuscar.Location = new System.Drawing.Point(12, 12);
+            this.gbBuscar.Name = "gbBuscar";
+            this.gbBuscar.Size = new System.Drawing.Size(660, 50);
+            this.gbBuscar.TabIndex = 0;
+            this.gbBuscar.TabStop = false;
+            this.gbBuscar.Text = "Buscar";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(225, 18);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 25);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(15, 19);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(200, 20);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // dgvLaboratorios
+            // 
+            this.dgvLaboratorios.AllowUserToAddRows = false;
+            this.dgvLaboratorios.AllowUserToDeleteRows = false;
+            this.dgvLaboratorios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLaboratorios.Location = new System.Drawing.Point(12, 70);
+            this.dgvLaboratorios.MultiSelect = false;
+            this.dgvLaboratorios.Name = "dgvLaboratorios";
+            this.dgvLaboratorios.ReadOnly = true;
+            this.dgvLaboratorios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullColumnSelect;
+            this.dgvLaboratorios.Size = new System.Drawing.Size(660, 150);
+            this.dgvLaboratorios.TabIndex = 1;
+            this.dgvLaboratorios.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLaboratorios_CellClick);
+            // 
+            // gbDatos
+            // 
+            this.gbDatos.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.gbDatos.Controls.Add(this.btnEliminar);
+            this.gbDatos.Controls.Add(this.btnEditar);
+            this.gbDatos.Controls.Add(this.btnGuardar);
+            this.gbDatos.Controls.Add(this.btnNuevo);
+            this.gbDatos.Controls.Add(this.txtPais);
+            this.gbDatos.Controls.Add(this.txtNombre);
+            this.gbDatos.Controls.Add(this.label2);
+            this.gbDatos.Controls.Add(this.label1);
+            this.gbDatos.Location = new System.Drawing.Point(12, 230);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(660, 120);
+            this.gbDatos.TabIndex = 2;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Datos del Laboratorio";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(20, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\tNombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(20, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "País:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(100, 27);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(250, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtPais
+            // 
+            this.txtPais.Location = new System.Drawing.Point(100, 57);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(200, 20);
+            this.txtPais.TabIndex = 3;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(300, 85);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(80, 30);
+            this.btnNuevo.TabIndex = 4;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(390, 85);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(80, 30);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(480, 85);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(80, 30);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(570, 85);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(80, 30);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // FrmLaboratorios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(684, 411);
+            this.Controls.Add(this.gbDatos);
+            this.Controls.Add(this.dgvLaboratorios);
+            this.Controls.Add(this.gbBuscar);
+            this.Name = "FrmLaboratorios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "\tGestión de Laboratorios";
+            this.Load += new System.EventHandler(this.FrmLaboratorios_Load);
+            this.gbBuscar.ResumeLayout(false);
+            this.gbBuscar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLaboratorios)).EndInit();
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dgvLaboratorios;
+        private System.Windows.Forms.GroupBox gbDatos;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
---
+++ Farmacia/CpFarmacia/FrmLaboratorios.cs
@@ -0,0 +1,185 @@
+﻿using CadFarmacia;
+using ClnFarmacia;
+using System;
+using System.Windows.Forms;
+
+namespace CpFarmacia
+{
+    public partial class FrmLaboratorios : Form
+    {
+        private int idLaboratorioSeleccionado = 0;
+        private bool esNuevo = false;
+
+        public FrmLaboratorios()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLaboratorios_Load(object sender, EventArgs e)
+        {
+            Listar();
+            EstadoInicial();
+        }
+
+        private void Listar()
+        {
+            dgvLaboratorios.DataSource = LaboratorioCln.Listar(txtBuscar.Text.Trim());
+            FormatearGrilla();
+        }
+
+        private void FormatearGrilla()
+        {
+            if (dgvLaboratorios.Columns.Count > 0)
+            {
+                dgvLaboratorios.Columns["id"].Visible = false;
+                dgvLaboratorios.Columns["nombre"].HeaderText = "Nombre";
+                dgvLaboratorios.Columns["pais"].HeaderText = "País";
+                dgvLaboratorios.Columns["estado"].HeaderText = "Estado";
+            }
+        }
+
+        private void EstadoInicial()
+        {
+            txtNombre.Clear();
+            txtPais.Clear();
+
+            txtNombre.Enabled = false;
+            txtPais.Enabled = false;
+
+            btnGuardar.Enabled = false;
+            btnEditar.Enabled = true;
+            btnEliminar.Enabled = true;
+            btnNuevo.Enabled = true;
+
+            idLaboratorioSeleccionado = 0;
+            esNuevo = false;
+        }
+
+        private void HabilitarCampos()
+        {
+            txtNombre.Enabled = true;
+            txtPais.Enabled = true;
+
+            btnGuardar.Enabled = true;
+            btnEditar.Enabled = false;
+            btnEliminar.Enabled = false;
+
+            txtNombre.Focus();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            esNuevo = true;
+            idLaboratorioSeleccionado = 0;
+            txtNombre.Clear();
+            txtPais.Clear();
+            HabilitarCampos();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("Ingrese el nombre del laboratorio", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNombre.Focus();
+                return;
+            }
+
+            try
+            {
+                if (esNuevo)
+                {
+                    Laboratorio laboratorio = new Laboratorio
+                    {
+                        nombre = txtNombre.Text.Trim(),
+                        pais = txtPais.Text.Trim(),
+                        estado = 1
+                    };
+
+                    LaboratorioCln.Insertar(laboratorio);
+                    MessageBox.Show("Laboratorio registrado correctamente", "Éxito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    Laboratorio laboratorio = LaboratorioCln.ObtenerPorId(idLaboratorioSeleccionado);
+                    laboratorio.nombre = txtNombre.Text.Trim();
+                    laboratorio.pais = txtPais.Text.Trim();
+
+                    LaboratorioCln.Actualizar(laboratorio);
+                    MessageBox.Show("Laboratorio actualizado correctamente", "Éxito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                Listar();
+                EstadoInicial();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (idLaboratorioSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un laboratorio de la lista", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            esNuevo = false;
+            HabilitarCampos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (idLaboratorioSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un laboratorio de la lista", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("¿Está seguro de eliminar este laboratorio?",
+                "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    LaboratorioCln.Eliminar(idLaboratorioSeleccionado);
+                    MessageBox.Show("Laboratorio eliminado correctamente", "Éxito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Listar();
+                    EstadoInicial();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al eliminar: " + ex.Message, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void dgvLaboratorios_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dgvLaboratorios.Rows[e.RowIndex];
+
+                idLaboratorioSeleccionado = Convert.ToInt32(fila.Cells["id"].Value);
+                txtNombre.Text = fila.Cells["nombre"].Value.ToString();
+                txtPais.Text = fila.Cells["pais"].Value?.ToString() ?? "";
+            }
+        }
+    }
+}
