5f8d1f8e5ea5c9f1cabae9e41ded5bf05b6ce818
Manuel Adolfo Soto <sotomanueladolfo1@gmail.com>
2025-11-26 13:00:00 -0400

    Se Mejoro FrmMedicamentos

--- Farmacia/CpFarmacia/FrmMedicamentos.Designer.cs
+++ Farmacia/CpFarmacia/FrmMedicamentos.Designer.cs
@@ -43,6 +43,19 @@
             this.txtDescripcion = new System.Windows.Forms.TextBox();
             this.txtComposicion = new System.Windows.Forms.TextBox();
             this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cboLaboratorio = new System.Windows.Forms.ComboBox();
+            this.txtPrecioVenta = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
+            this.chkRequiereReceta = new System.Windows.Forms.CheckBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
             this.gbBuscar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMedicamentos)).BeginInit();
             this.gbDatos.SuspendLayout();
@@ -75,6 +88,7 @@
             this.btnBuscar.TabIndex = 2;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // dgvMedicamentos
             // 
@@ -88,10 +102,24 @@
             this.dgvMedicamentos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvMedicamentos.Size = new System.Drawing.Size(910, 180);
             this.dgvMedicamentos.TabIndex = 1;
+            this.dgvMedicamentos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMedicamentos_CellClick);
             // 
             // gbDatos
             // 
             this.gbDatos.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.gbDatos.Controls.Add(this.btnEliminar);
+            this.gbDatos.Controls.Add(this.btnEditar);
+            this.gbDatos.Controls.Add(this.btnGuardar);
+            this.gbDatos.Controls.Add(this.btnNuevo);
+            this.gbDatos.Controls.Add(this.chkRequiereReceta);
+            this.gbDatos.Controls.Add(this.dtpFechaVencimiento);
+            this.gbDatos.Controls.Add(this.txtStock);
+            this.gbDatos.Controls.Add(this.txtPrecioVenta);
+            this.gbDatos.Controls.Add(this.cboLaboratorio);
+            this.gbDatos.Controls.Add(this.label9);
+            this.gbDatos.Controls.Add(this.label8);
+            this.gbDatos.Controls.Add(this.label7);
+            this.gbDatos.Controls.Add(this.label6);
             this.gbDatos.Controls.Add(this.cboCategoria);
             this.gbDatos.Controls.Add(this.txtComposicion);
             this.gbDatos.Controls.Add(this.txtDescripcion);
@@ -196,6 +224,127 @@
             this.cboCategoria.Size = new System.Drawing.Size(200, 21);
             this.cboCategoria.TabIndex = 9;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(480, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Laboratorio:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(480, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Precio Venta:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label8.Location = new System.Drawing.Point(480, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Stock:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(480, 120);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Fecha Venc.:";
+            // 
+            // cboLaboratorio
+            // 
+            this.cboLaboratorio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboLaboratorio.FormattingEnabled = true;
+            this.cboLaboratorio.Location = new System.Drawing.Point(580, 27);
+            this.cboLaboratorio.Name = "cboLaboratorio";
+            this.cboLaboratorio.Size = new System.Drawing.Size(200, 21);
+            this.cboLaboratorio.TabIndex = 14;
+            // 
+            // txtPrecioVenta
+            // 
+            this.txtPrecioVenta.Location = new System.Drawing.Point(580, 57);
+            this.txtPrecioVenta.Name = "txtPrecioVenta";
+            this.txtPrecioVenta.Size = new System.Drawing.Size(580, 20);
+            this.txtPrecioVenta.TabIndex = 15;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(580, 87);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 16;
+            // 
+            // dtpFechaVencimiento
+            // 
+            this.dtpFechaVencimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaVencimiento.Location = new System.Drawing.Point(580, 117);
+            this.dtpFechaVencimiento.Name = "dtpFechaVencimiento";
+            this.dtpFechaVencimiento.Size = new System.Drawing.Size(150, 20);
+            this.dtpFechaVencimiento.TabIndex = 17;
+            // 
+            // chkRequiereReceta
+            // 
+            this.chkRequiereReceta.AutoSize = true;
+            this.chkRequiereReceta.Location = new System.Drawing.Point(580, 150);
+            this.chkRequiereReceta.Name = "chkRequiereReceta";
+            this.chkRequiereReceta.Size = new System.Drawing.Size(107, 17);
+            this.chkRequiereReceta.TabIndex = 18;
+            this.chkRequiereReceta.Text = "Requiere Receta";
+            this.chkRequiereReceta.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(500, 190);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(90, 35);
+            this.btnNuevo.TabIndex = 19;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(600, 190);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(90, 35);
+            this.btnGuardar.TabIndex = 20;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(700, 190);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(90, 35);
+            this.btnEditar.TabIndex = 21;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(800, 190);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(90, 35);
+            this.btnEliminar.TabIndex = 22;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
             // FrmMedicamentos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -208,6 +357,7 @@
             this.Name = "FrmMedicamentos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Medicamentos";
+            this.Load += new System.EventHandler(this.FrmMedicamentos_Load);
             this.gbBuscar.ResumeLayout(false);
             this.gbBuscar.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMedicamentos)).EndInit();
@@ -234,5 +384,18 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.ComboBox cboLaboratorio;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtPrecioVenta;
+        private System.Windows.Forms.CheckBox chkRequiereReceta;
+        private System.Windows.Forms.DateTimePicker dtpFechaVencimiento;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnNuevo;
     }
 }
--- Farmacia/CpFarmacia/FrmMedicamentos.cs
+++ Farmacia/CpFarmacia/FrmMedicamentos.cs
@@ -1,20 +1,316 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CadFarmacia;
+using ClnFarmacia;
+using System;
 using System.Windows.Forms;
 
 namespace CpFarmacia
 {
     public partial class FrmMedicamentos : Form
     {
+        private int idMedicamentoSeleccionado = 0;
+        private bool esNuevo = false;
+
         public FrmMedicamentos()
         {
             InitializeComponent();
+        }
+
+        private void FrmMedicamentos_Load(object sender, EventArgs e)
+        {
+            CargarCombos();
+            Listar();
+            EstadoInicial();
+        }
+
+        private void CargarCombos()
+        {
+            // Cargar Categorías
+            cboCategoria.DataSource = CategoriaCln.Listar();
+            cboCategoria.DisplayMember = "nombre";
+            cboCategoria.ValueMember = "id";
+            cboCategoria.SelectedIndex = -1;
+
+            // Cargar Laboratorios
+            cboLaboratorio.DataSource = LaboratorioCln.ListarActivos();
+            cboLaboratorio.DisplayMember = "nombre";
+            cboLaboratorio.ValueMember = "id";
+            cboLaboratorio.SelectedIndex = -1;
+        }
+
+        private void Listar()
+        {
+            dgvMedicamentos.DataSource = MedicamentoCln.Listar(txtBuscar.Text.Trim());
+            FormatearGrilla();
+        }
+
+        private void FormatearGrilla()
+        {
+            if (dgvMedicamentos.Columns.Count > 0)
+            {
+                dgvMedicamentos.Columns["id"].Visible = false;
+                dgvMedicamentos.Columns["codigo"].HeaderText = "Código";
+                dgvMedicamentos.Columns["nombre"].HeaderText = "Nombre";
+                dgvMedicamentos.Columns["descripcion"].HeaderText = "Descripción";
+                dgvMedicamentos.Columns["composicion"].Visible = false;
+                dgvMedicamentos.Columns["Categoria"].HeaderText = "Categoría";
+                dgvMedicamentos.Columns["Laboratorio"].HeaderText = "Laboratorio";
+                dgvMedicamentos.Columns["fechaVencimiento"].HeaderText = "Fecha Venc.";
+                dgvMedicamentos.Columns["stock"].HeaderText = "Stock";
+                dgvMedicamentos.Columns["precioVenta"].HeaderText = "Precio";
+                dgvMedicamentos.Columns["requiereReceta"].HeaderText = "Receta";
+                dgvMedicamentos.Columns["usuarioRegistro"].Visible = false;
+                dgvMedicamentos.Columns["fechaRegistro"].Visible = false;
+                dgvMedicamentos.Columns["estado"].HeaderText = "Estado";
+                dgvMedicamentos.Columns["idCategoria"].Visible = false;
+                dgvMedicamentos.Columns["idLaboratorio"].Visible = false;
+            }
+        }
+
+        private void EstadoInicial()
+        {
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            txtComposicion.Clear();
+            txtPrecioVenta.Clear();
+            txtStock.Clear();
+            cboCategoria.SelectedIndex = -1;
+            cboLaboratorio.SelectedIndex = -1;
+            dtpFechaVencimiento.Value = DateTime.Now.AddYears(1);
+            chkRequiereReceta.Checked = false;
+
+            txtCodigo.Enabled = false;
+            txtNombre.Enabled = false;
+            txtDescripcion.Enabled = false;
+            txtComposicion.Enabled = false;
+            txtPrecioVenta.Enabled = false;
+            txtStock.Enabled = false;
+            cboCategoria.Enabled = false;
+            cboLaboratorio.Enabled = false;
+            dtpFechaVencimiento.Enabled = false;
+            chkRequiereReceta.Enabled = false;
+
+            btnGuardar.Enabled = false;
+            btnEditar.Enabled = true;
+            btnEliminar.Enabled = true;
+            btnNuevo.Enabled = true;
+
+            idMedicamentoSeleccionado = 0;
+            esNuevo = false;
+        }
+
+        private void HabilitarCampos()
+        {
+            txtCodigo.Enabled = true;
+            txtNombre.Enabled = true;
+            txtDescripcion.Enabled = true;
+            txtComposicion.Enabled = true;
+            txtPrecioVenta.Enabled = true;
+            txtStock.Enabled = true;
+            cboCategoria.Enabled = true;
+            cboLaboratorio.Enabled = true;
+            dtpFechaVencimiento.Enabled = true;
+            chkRequiereReceta.Enabled = true;
+
+            btnGuardar.Enabled = true;
+            btnEditar.Enabled = false;
+            btnEliminar.Enabled = false;
+
+            txtCodigo.Focus();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            esNuevo = true;
+            idMedicamentoSeleccionado = 0;
+
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            txtComposicion.Clear();
+            txtPrecioVenta.Clear();
+            txtStock.Clear();
+            cboCategoria.SelectedIndex = -1;
+            cboLaboratorio.SelectedIndex = -1;
+            dtpFechaVencimiento.Value = DateTime.Now.AddYears(1);
+            chkRequiereReceta.Checked = false;
+
+            HabilitarCampos();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Validaciones
+            if (string.IsNullOrWhiteSpace(txtCodigo.Text))
+            {
+                MessageBox.Show("Ingrese el código", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtCodigo.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("Ingrese el nombre", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNombre.Focus();
+                return;
+            }
+
+            if (cboCategoria.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione una categoría", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cboCategoria.Focus();
+                return;
+            }
+
+            if (cboLaboratorio.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione un laboratorio", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cboLaboratorio.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtPrecioVenta.Text))
+            {
+                MessageBox.Show("Ingrese el precio de venta", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPrecioVenta.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtStock.Text))
+            {
+                MessageBox.Show("Ingrese el stock", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtStock.Focus();
+                return;
+            }
+
+            try
+            {
+                if (esNuevo)
+                {
+                    Medicamento medicamento = new Medicamento
+                    {
+                        codigo = txtCodigo.Text.Trim(),
+                        nombre = txtNombre.Text.Trim(),
+                        descripcion = txtDescripcion.Text.Trim(),
+                        composicion = txtComposicion.Text.Trim(),
+                        idCategoria = Convert.ToInt32(cboCategoria.SelectedValue),
+                        idLaboratorio = Convert.ToInt32(cboLaboratorio.SelectedValue),
+                        precioVenta = Convert.ToDecimal(txtPrecioVenta.Text),
+                        stock = Convert.ToInt32(txtStock.Text),
+                        fechaVencimiento = dtpFechaVencimiento.Value,
+                        requiereReceta = chkRequiereReceta.Checked,
+                        estado = 1
+                    };
+
+                    MedicamentoCln.Insertar(medicamento);
+                    MessageBox.Show("Medicamento registrado correctamente", "Éxito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    Medicamento medicamento = MedicamentoCln.ObtenerPorId(idMedicamentoSeleccionado);
+                    medicamento.codigo = txtCodigo.Text.Trim();
+                    medicamento.nombre = txtNombre.Text.Trim();
+                    medicamento.descripcion = txtDescripcion.Text.Trim();
+                    medicamento.composicion = txtComposicion.Text.Trim();
+                    medicamento.idCategoria = Convert.ToInt32(cboCategoria.SelectedValue);
+                    medicamento.idLaboratorio = Convert.ToInt32(cboLaboratorio.SelectedValue);
+                    medicamento.precioVenta = Convert.ToDecimal(txtPrecioVenta.Text);
+                    medicamento.stock = Convert.ToInt32(txtStock.Text);
+                    medicamento.fechaVencimiento = dtpFechaVencimiento.Value;
+                    medicamento.requiereReceta = chkRequiereReceta.Checked;
+
+                    MedicamentoCln.Actualizar(medicamento);
+                    MessageBox.Show("Medicamento actualizado correctamente", "Éxito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                Listar();
+                EstadoInicial();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (idMedicamentoSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un medicamento de la lista", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            esNuevo = false;
+            HabilitarCampos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (idMedicamentoSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un medicamento de la lista", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("¿Está seguro de eliminar este medicamento?",
+                "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    MedicamentoCln.Eliminar(idMedicamentoSeleccionado);
+                    MessageBox.Show("Medicamento eliminado correctamente", "Éxito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Listar();
+                    EstadoInicial();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al eliminar: " + ex.Message, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void dgvMedicamentos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dgvMedicamentos.Rows[e.RowIndex];
+
+                idMedicamentoSeleccionado = Convert.ToInt32(fila.Cells["id"].Value);
+                txtCodigo.Text = fila.Cells["codigo"].Value.ToString();
+                txtNombre.Text = fila.Cells["nombre"].Value.ToString();
+                txtDescripcion.Text = fila.Cells["descripcion"].Value?.ToString() ?? "";
+                txtComposicion.Text = fila.Cells["composicion"].Value?.ToString() ?? "";
+                txtPrecioVenta.Text = fila.Cells["precioVenta"].Value.ToString();
+                txtStock.Text = fila.Cells["stock"].Value.ToString();
+
+                // Seleccionar en ComboBox
+                cboCategoria.SelectedValue = Convert.ToInt32(fila.Cells["idCategoria"].Value);
+                cboLaboratorio.SelectedValue = Convert.ToInt32(fila.Cells["idLaboratorio"].Value);
+
+                // Fecha y checkbox
+                dtpFechaVencimiento.Value = Convert.ToDateTime(fila.Cells["fechaVencimiento"].Value);
+                chkRequiereReceta.Checked = Convert.ToBoolean(fila.Cells["requiereReceta"].Value);
+            }
         }
     }
 }
