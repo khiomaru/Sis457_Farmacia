7620a7d23fef06b7015e5733353ad73315b6b307
Manuel Adolfo Soto <sotomanueladolfo1@gmail.com>
2025-11-26 22:54:03 -0400

    Se agrego FrmListaVentas

---
+++ Farmacia/CpFarmacia/FrmListaVentas.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace CpFarmacia
+{
+    partial class FrmListaVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbBuscar = new System.Windows.Forms.GroupBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dgvVentas = new System.Windows.Forms.DataGridView();
+            this.gbDetalle = new System.Windows.Forms.GroupBox();
+            this.dgvDetalle = new System.Windows.Forms.DataGridView();
+            this.btnVerDetalle = new System.Windows.Forms.Button();
+            this.btnAnular = new System.Windows.Forms.Button();
+            this.gbBuscar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).BeginInit();
+            this.gbDetalle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbBuscar
+            // 
+            this.gbBuscar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.gbBuscar.Controls.Add(this.btnBuscar);
+            this.gbBuscar.Controls.Add(this.txtBuscar);
+            this.gbBuscar.Location = new System.Drawing.Point(12, 12);
+            this.gbBuscar.Name = "gbBuscar";
+            this.gbBuscar.Size = new System.Drawing.Size(910, 50);
+            this.gbBuscar.TabIndex = 0;
+            this.gbBuscar.TabStop = false;
+            this.gbBuscar.Text = "Buscar";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(15, 19);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(250, 20);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(275, 18);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 25);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // dgvVentas
+            // 
+            this.dgvVentas.AllowUserToAddRows = false;
+            this.dgvVentas.AllowUserToDeleteRows = false;
+            this.dgvVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVentas.Location = new System.Drawing.Point(12, 70);
+            this.dgvVentas.MultiSelect = false;
+            this.dgvVentas.Name = "dgvVentas";
+            this.dgvVentas.ReadOnly = true;
+            this.dgvVentas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvVentas.Size = new System.Drawing.Size(910, 200);
+            this.dgvVentas.TabIndex = 1;
+            this.dgvVentas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVentas_CellClick);
+            // 
+            // gbDetalle
+            // 
+            this.gbDetalle.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.gbDetalle.Controls.Add(this.dgvDetalle);
+            this.gbDetalle.Location = new System.Drawing.Point(12, 280);
+            this.gbDetalle.Name = "gbDetalle";
+            this.gbDetalle.Size = new System.Drawing.Size(910, 170);
+            this.gbDetalle.TabIndex = 2;
+            this.gbDetalle.TabStop = false;
+            this.gbDetalle.Text = "Detalle de la Venta";
+            // 
+            // dgvDetalle
+            // 
+            this.dgvDetalle.AllowUserToAddRows = false;
+            this.dgvDetalle.AllowUserToDeleteRows = false;
+            this.dgvDetalle.BackgroundColor = System.Drawing.Color.RosyBrown;
+            this.dgvDetalle.GridColor = System.Drawing.Color.Black;
+            this.dgvDetalle.Location = new System.Drawing.Point(10, 20);
+            this.dgvDetalle.Name = "dgvDetalle";
+            this.dgvDetalle.ReadOnly = true;
+            this.dgvDetalle.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDetalle.Size = new System.Drawing.Size(890, 140);
+            this.dgvDetalle.TabIndex = 0;
+            // 
+            // btnVerDetalle
+            // 
+            this.btnVerDetalle.Location = new System.Drawing.Point(700, 460);
+            this.btnVerDetalle.Name = "btnVerDetalle";
+            this.btnVerDetalle.Size = new System.Drawing.Size(100, 35);
+            this.btnVerDetalle.TabIndex = 3;
+            this.btnVerDetalle.Text = "Ver Detalle";
+            this.btnVerDetalle.UseVisualStyleBackColor = true;
+            this.btnVerDetalle.Click += new System.EventHandler(this.btnVerDetalle_Click);
+            // 
+            // btnAnular
+            // 
+            this.btnAnular.Location = new System.Drawing.Point(810, 460);
+            this.btnAnular.Name = "btnAnular";
+            this.btnAnular.Size = new System.Drawing.Size(110, 35);
+            this.btnAnular.TabIndex = 4;
+            this.btnAnular.Text = "Anular Venta";
+            this.btnAnular.UseVisualStyleBackColor = true;
+            this.btnAnular.Click += new System.EventHandler(this.btnAnular_Click);
+            // 
+            // FrmListaVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(934, 511);
+            this.Controls.Add(this.btnAnular);
+            this.Controls.Add(this.btnVerDetalle);
+            this.Controls.Add(this.gbDetalle);
+            this.Controls.Add(this.dgvVentas);
+            this.Controls.Add(this.gbBuscar);
+            this.Name = "FrmListaVentas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista de Ventas";
+            this.Load += new System.EventHandler(this.FrmListaVentas_Load);
+            this.gbBuscar.ResumeLayout(false);
+            this.gbBuscar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).EndInit();
+            this.gbDetalle.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dgvVentas;
+        private System.Windows.Forms.GroupBox gbDetalle;
+        private System.Windows.Forms.DataGridView dgvDetalle;
+        private System.Windows.Forms.Button btnVerDetalle;
+        private System.Windows.Forms.Button btnAnular;
+    }
+}
---
+++ Farmacia/CpFarmacia/FrmListaVentas.cs
@@ -0,0 +1,169 @@
+﻿using CadFarmacia;
+using ClnFarmacia;
+using System;
+using System.Windows.Forms;
+
+namespace CpFarmacia
+{
+    public partial class FrmListaVentas : Form
+    {
+        private int idVentaSeleccionada = 0;
+
+        public FrmListaVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmListaVentas_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void Listar()
+        {
+            dgvVentas.DataSource = VentaCln.Listar(txtBuscar.Text.Trim());
+            FormatearGrillaVentas();
+            LimpiarDetalle();
+        }
+
+        private void FormatearGrillaVentas()
+        {
+            if (dgvVentas.Columns.Count > 0)
+            {
+                dgvVentas.Columns["id"].Visible = false;
+                dgvVentas.Columns["numeroFactura"].HeaderText = "Nro. Factura";
+                dgvVentas.Columns["numeroFactura"].Width = 120;
+                dgvVentas.Columns["Cliente"].HeaderText = "Cliente";
+                dgvVentas.Columns["Cliente"].Width = 200;
+                dgvVentas.Columns["Usuario"].HeaderText = "Usuario";
+                dgvVentas.Columns["Usuario"].Width = 120;
+                dgvVentas.Columns["total"].HeaderText = "Total";
+                dgvVentas.Columns["total"].Width = 100;
+                dgvVentas.Columns["total"].DefaultCellStyle.Format = "N2";
+                dgvVentas.Columns["fechaVenta"].HeaderText = "Fecha";
+                dgvVentas.Columns["fechaVenta"].Width = 150;
+                dgvVentas.Columns["estado"].HeaderText = "Estado";
+                dgvVentas.Columns["estado"].Width = 80;
+            }
+        }
+
+        private void LimpiarDetalle()
+        {
+            dgvDetalle.DataSource = null;
+            idVentaSeleccionada = 0;
+        }
+
+        private void CargarDetalle(int idVenta)
+        {
+            var detalles = DetalleVentaCln.ObtenerPorVenta(idVenta);
+
+            var listaDetalle = new System.Collections.Generic.List<object>();
+            foreach (var d in detalles)
+            {
+                listaDetalle.Add(new
+                {
+                    codigo = d.Medicamento.codigo,
+                    medicamento = d.Medicamento.nombre,
+                    precioUnitario = d.precioUnitario,
+                    cantidad = d.cantidad,
+                    subtotal = d.subtotal
+                });
+            }
+
+            dgvDetalle.DataSource = listaDetalle;
+            FormatearGrillaDetalle();
+        }
+
+        private void FormatearGrillaDetalle()
+        {
+            if (dgvDetalle.Columns.Count > 0)
+            {
+                dgvDetalle.Columns["codigo"].HeaderText = "Código";
+                dgvDetalle.Columns["codigo"].Width = 100;
+                dgvDetalle.Columns["medicamento"].HeaderText = "Medicamento";
+                dgvDetalle.Columns["medicamento"].Width = 300;
+                dgvDetalle.Columns["precioUnitario"].HeaderText = "Precio Unit.";
+                dgvDetalle.Columns["precioUnitario"].Width = 100;
+                dgvDetalle.Columns["precioUnitario"].DefaultCellStyle.Format = "N2";
+                dgvDetalle.Columns["cantidad"].HeaderText = "Cantidad";
+                dgvDetalle.Columns["cantidad"].Width = 80;
+                dgvDetalle.Columns["subtotal"].HeaderText = "Subtotal";
+                dgvDetalle.Columns["subtotal"].Width = 120;
+                dgvDetalle.Columns["subtotal"].DefaultCellStyle.Format = "N2";
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void dgvVentas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dgvVentas.Rows[e.RowIndex];
+                idVentaSeleccionada = Convert.ToInt32(fila.Cells["id"].Value);
+                CargarDetalle(idVentaSeleccionada);
+            }
+        }
+
+        private void btnVerDetalle_Click(object sender, EventArgs e)
+        {
+            if (idVentaSeleccionada == 0)
+            {
+                MessageBox.Show("Seleccione una venta de la lista", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            CargarDetalle(idVentaSeleccionada);
+        }
+
+        private void btnAnular_Click(object sender, EventArgs e)
+        {
+            if (idVentaSeleccionada == 0)
+            {
+                MessageBox.Show("Seleccione una venta de la lista", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Verificar que la venta no esté ya anulada
+            if (dgvVentas.CurrentRow != null)
+            {
+                int estado = Convert.ToInt32(dgvVentas.CurrentRow.Cells["estado"].Value);
+                if (estado == 0)
+                {
+                    MessageBox.Show("Esta venta ya está anulada", "Validación",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            DialogResult result = MessageBox.Show(
+                "¿Está seguro de anular esta venta?\n\nEsta acción devolverá el stock de los medicamentos.",
+                "Confirmar anulación",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    VentaCln.Anular(idVentaSeleccionada);
+
+                    MessageBox.Show("Venta anulada correctamente", "Éxito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    Listar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al anular la venta: " + ex.Message, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
