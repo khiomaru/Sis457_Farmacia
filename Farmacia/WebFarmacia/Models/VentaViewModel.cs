5020b6731c1ccdddb9699b932148a660ffe84ee2
Manuel Adolfo Soto <sotomanueladolfo1@gmail.com>
2025-11-30 12:27:37 -0400

    Se arreglaron errores de logica

--- Farmacia/WebFarmacia/Models/VentaViewModel.cs
+++ Farmacia/WebFarmacia/Models/VentaViewModel.cs
@@ -4,13 +4,13 @@ namespace WebFarmacia.Models
 {
     public class VentaViewModel
     {
-        public string TipoDocumento { get; set; }
-        public string DocumentoCliente { get; set; }
-        public string NombreCliente { get; set; }
+        public string TipoDocumento { get; set; } = string.Empty;
+        public string DocumentoCliente { get; set; } = string.Empty;
+        public string NombreCliente { get; set; } = string.Empty;
         public decimal MontoPago { get; set; }
         public decimal MontoCambio { get; set; }
         public decimal MontoTotal { get; set; }
-        public List<VentaDetalleDTO> VentaDetalles { get; set; }
+        public List<VentaDetalleDTO> VentaDetalles { get; set; } = [];
     }
 
     public class VentaDetalleDTO
